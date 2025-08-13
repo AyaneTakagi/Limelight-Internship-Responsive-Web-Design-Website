f8c3c508a6e687eade653f1a51d7b0158292f562
Ayane Takagi <ayane1123@mailbox.unideb.hu>
2025-08-14 06:20:10 +0900

    Add page components and basic routing for Home, About, Projects, Certificates, and Contact

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Certificates from './Certificates';
+import Contact from './Contact';
+
+export { Home, About, Projects, Certificates, Contact };
